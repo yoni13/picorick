381a03be7b53016c7d9411df8762e78540d178ce
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-12-04 17:36:36 +0800

    Update app.py

--- pico/app.py
+++ pico/app.py
@@ -5,7 +5,6 @@ import urequests as requests
 print('init success')
 led = machine.Pin("LED", machine.Pin.OUT)
 def connect():
-    led.on()
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect('SGGS_CS',"awawa87b")
@@ -14,50 +13,57 @@ def connect():
         sleep(1)
     print('connected to wifi')
 connect()
-led.off()
+led.on()
 while True:
-        led.off()
         r26 = machine.ADC(26).read_u16()
         r27 = machine.ADC(27).read_u16()
         r28 = machine.ADC(28).read_u16()
         # r < 200 = 1
         if r26 > 200 and r27 < 200 and r28 > 200:#rickroll is 0 1 0
+            led.off()
+            sleep(5)
             requests.get('http://192.168.1.107:5000/rickroll')
-            print(str(r26) +' '+ str(r27) +' '+ str(r28))
             print('rickroll')
             sleep(5)
             
         if r26 < 200 and r27 > 200 and r28 > 200:#another rickroll is 1 0 0
+            led.off()
+            sleep(5)
             requests.get('http://192.168.1.107:5000/anotherrickroll')
-            print(str(r26) +' '+ str(r27) +' '+ str(r28))
             print('another rickroll')
             sleep(5)
             
         if r26 > 200 and r27 > 200 and r28 < 200:#esc is 0 0 1
+            led.off()
+            sleep(5)
             requests.get('http://192.168.1.107:5000/esc')
-            print(str(r26) +' '+ str(r27) +' '+ str(r28))
             print('esc')
             sleep(5)
             
         if r26 < 200 and r27 < 200 and r28 > 200:#herwegoagain is 1 1 0
+            led.off()
+            sleep(5)
             requests.get('http://192.168.1.107:5000/herewegoagain')
             print('here we go again')
-            print(str(r26) +' '+ str(r27) +' '+ str(r28))
             sleep(5)
             
         if r26 < 200 and r27 < 200 and r28 < 200:#hh is 1 1 1
+            led.off()
+            sleep(5)
             requests.get('http://192.168.1.107:5000/hh')
-            print(str(r26) +' '+ str(r27) +' '+ str(r28))
             print('hh')
             sleep(5)
             
         if r26 > 200 and r27 < 200 and r28 < 200:#4xuan is 0 1 1
+            print('awa'+str(r26) +' '+ str(r27) +' '+ str(r28))
+            led.off()
+            sleep(5)
             requests.get('http://192.168.1.107:5000/4xuan')
-            print(str(r26) +' '+ str(r27) +' '+ str(r28))
             print('4xuan')
             sleep(5)
         print(str(r26) +' '+ str(r27) +' '+ str(r28))
         del r26,r27,r28
         led.on()
-        sleep(1.5)
+        sleep(0.8)
+        
 

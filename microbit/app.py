24d98cd7ebd2e97ab2ad07651cb828b67af6d5b7
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-12-04 17:37:28 +0800

    Update app.py

--- microbit/app.py
+++ microbit/app.py
@@ -7,10 +7,35 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    pins.analog_write_pin(AnalogPin.P1, pinlist[mode])
+    if pinlist[mode] == 1:
+        pins.analog_write_pin(AnalogPin.P0, 1000)
+        pins.analog_write_pin(AnalogPin.P1, 1000)
+        pins.analog_write_pin(AnalogPin.P2, 1)
+    if pinlist[mode] == 2:
+        pins.analog_write_pin(AnalogPin.P0, 1000)
+        pins.analog_write_pin(AnalogPin.P1, 1)
+        pins.analog_write_pin(AnalogPin.P2, 1000)
+    if pinlist[mode] == 3:
+        pins.analog_write_pin(AnalogPin.P0, 1)
+        pins.analog_write_pin(AnalogPin.P1, 1000)
+        pins.analog_write_pin(AnalogPin.P2, 1000)
+    if pinlist[mode] == 4:
+        pins.analog_write_pin(AnalogPin.P0, 1)
+        pins.analog_write_pin(AnalogPin.P1, 1)
+        pins.analog_write_pin(AnalogPin.P2, 1000)
+    if pinlist[mode] == 5:
+        pins.analog_write_pin(AnalogPin.P0, 1)
+        pins.analog_write_pin(AnalogPin.P1, 1)
+        pins.analog_write_pin(AnalogPin.P2, 1)
+    if pinlist[mode] == 6:
+        pins.analog_write_pin(AnalogPin.P0, 1000)
+        pins.analog_write_pin(AnalogPin.P1, 1)
+        pins.analog_write_pin(AnalogPin.P2, 1)
     basic.show_string("writed" + ("" + str(pinlist[mode])))
     basic.pause(1000)
-    pins.analog_write_pin(AnalogPin.P1, 0)
+    pins.analog_write_pin(AnalogPin.P0, 1000)
+    pins.analog_write_pin(AnalogPin.P1, 1000)
+    pins.analog_write_pin(AnalogPin.P2, 1000)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
@@ -21,16 +46,9 @@ def on_button_pressed_b():
     basic.show_string("" + (wordlist[mode]))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-connected = 0
 mode = 0
 pinlist: List[number] = []
 wordlist: List[str] = []
 wordlist = ["esc", "rick", "anorick", "again", "hh", "4xuan"]
 pinlist = [1, 2, 3, 4, 5, 6]
 mode = 0
-pins.analog_write_pin(AnalogPin.P0, 13)
-if pins.analog_read_pin(AnalogPin.P0) == 787:
-    connected = 1
-    basic.show_string("ok")
-else:
-    basic.show_string("failed")
